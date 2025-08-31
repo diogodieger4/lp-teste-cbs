c4a77cfd14276d7f21cc5012ba601996af2a0cee
diogodieger4 <joaonogueira91893@gmail.com>
2025-08-31 11:09:29 -0300

    cors error

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 fetch("/api/env")
-  .then(res => res.json())
-  .then(data => {
+  .then((res) => res.json())
+  .then((data) => {
     document.getElementById("banner").innerText = data.message;
   })
-  .catch(err => {
+  .catch((err) => {
     console.error("Erro ao buscar variável:", err);
+  });
+
+fetch("https://api.cbsoficial.com.br/users")
+  .then((res) => res.json)
+  .then((users) => {
+    const tbody = document.querySelector("#usersTable tbody");
+    tbody.innerHTML = "";
+
+    users.forEach((user) => {
+      const tr = document.createElement("tr");
+      tr.innerHTML = `
+        <td>${user.id}</td>
+        <td>${user.name || "-"}</td>
+        <td>${user.email || "-"}</td>
+        <td>${user.phone || "-"}</td>
+        <td>${new Date(user.created_at).toLocaleString("pt-BR")}</td>
+        `;
+    });
+  })
+  .catch((err) => {
+    console.error("Erro ao buscar usu'arios", err);
   });
